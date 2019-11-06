76dffd6681cb9fab8572fbcac034fcff412f2340
BrittneyHaselmayer <brittneyhaselmayer@yahoo.com>
2019-11-06 16:12:51 -0600

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES 
+  (1, "George", 1, "Male", "Brown", "Good", TRUE),
+  (2, "Sally", 2, "Female", "White", "Good", TRUE),
+  (3, "Tony", 5 , "Male", "Black", "Good", TRUE),
+  (4, "Joe", 10 , "Male", "Red", "Bad", FALSE),
+  (5, "Kelly", 7, "Female", "White", "Bad" , FALSE),
+  (6, "Brittney", 100, "Female", "Rainbow", "Amazing", TRUE),
+  (7, "Kev", 70, "Male", "Gray", "Terrible", FALSE),
+  (8, NULL, 30, "Female", "Black", "Good", TRUE);
