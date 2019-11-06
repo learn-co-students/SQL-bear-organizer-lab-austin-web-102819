d7fd476d2dcd08ef275955ab699ed7e2b649f1aa
Yining Wang <yining.wang@utexas.edu>
2019-11-06 13:24:00 -0600

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 3, "male", "brown", "mild", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 2, "male", "brown", "calm", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 1, "femail", "grey", "furious", FALSE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 3, "male", "brown", "furious", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 2, "female", "brown", "calm", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 5, "male", "brown", "happy", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 3, "female", "brown", "angry", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 3, "male", "brown", "killer", TRUE);
